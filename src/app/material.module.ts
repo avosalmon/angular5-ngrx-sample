c32a5e6a53bf0dac75ec0c17c730dea8d255ca12
Ryuta Hamasaki <avosalmon@gmail.com>
2018-02-03 23:22:44 +0900

    feat: implement app header

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const modules = [MatToolbarModule, MatButtonModule, MatIconModule];
+
+@NgModule({
+  imports: modules,
+  exports: modules
+})
+export class MaterialModule {}
