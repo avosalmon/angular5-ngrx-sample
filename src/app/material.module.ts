b3a649e0679a37b3626a8dae4a1391b65e8065d9
Ryuta Hamasaki <avosalmon@gmail.com>
2018-02-03 23:30:28 +0900

    feat: implement basic sidenav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-const modules = [MatToolbarModule, MatButtonModule, MatIconModule];
+const modules = [
+  MatSidenavModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+];
 
 @NgModule({
   imports: modules,
