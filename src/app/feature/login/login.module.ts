122f6b85f50afae15c6ffbe891a5f945052ef96e
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2018-02-17 14:52:56 +0900

    feat: implement login form

--- src/app/feature/login/login.module.ts
+++ src/app/feature/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LoginMaterialModule } from './login-material.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { LoginFormComponent } from './containers/login-form/login-form.component';
@@ -8,7 +9,8 @@ import { LoginFormComponent } from './containers/login-form/login-form.component
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    LoginMaterialModule
   ],
   declarations: [
     LoginComponent,
