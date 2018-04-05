557c8e563124a6b84477d7425177af7000df62e2
Ryuta Hamasaki <avosalmon@gmail.com>
2018-04-05 23:42:10 +0900

    feat: add loading property to ProductsState

--- src/app/feature/products/store/reducers/products.reducer.ts
+++ src/app/feature/products/store/reducers/products.reducer.ts
@@ -5,6 +5,7 @@ export interface ProductsState {
   list: any[];
   page: Page;
   searchQuery: string;
+  loading: boolean;
 }
 
 export const initialState: ProductsState = {
@@ -16,23 +17,33 @@ export const initialState: ProductsState = {
     direction: 'desc',
     total: 0
   },
-  searchQuery: ''
+  searchQuery: '',
+  loading: false
 };
 
 export function reducer(state = initialState, action: fromActions.ProductsAction): ProductsState {
   switch (action.type) {
+    case fromActions.GET_PRODUCTS: {
+      return {
+        ...state,
+        loading: true
+      };
+    }
+
     case fromActions.GET_PRODUCTS_SUCCESS: {
       return {
         ...state,
         list: action.payload.products,
-        page: action.payload.meta
+        page: action.payload.meta,
+        loading: false
       };
     }
 
     case fromActions.SEARCH_PRODUCTS: {
       return {
         ...state,
-        searchQuery: action.query
+        searchQuery: action.query,
+        loading: true
       };
     }
   }
